29af79d30ae7aa09d9672868063edaeba8628e94
Peehu Mishra <peehumishra1308@gmail.com>
2025-09-20 19:52:54 +0530

    Updates

---
+++ Models/salinity/model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+df=pd.read_csv("salinity/Indian_Ocean_surface_oce.tab",
+               sep="\t",
+               skiprows=19,
+               engine="python")
+
+print(df.head())
+print(df.info())
+print(df.shape)
+print("above i have printed the shape")
+print("Latitude range:", df["Latitude"].min(), "to", df["Latitude"].max())
+print("Longitude range:", df["Longitude"].min(), "to", df["Longitude"].max())
+print("Salinity range:", df["Sal"].min(), "to", df["Sal"].max())
+
+
+import matplotlib.pyplot as plt
+
+plt.scatter(df["Longitude"], df["Latitude"], c=df["Sal"], cmap="viridis", s=10)
+plt.colorbar(label="Salinity (PSU)")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.show()
